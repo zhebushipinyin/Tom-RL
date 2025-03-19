81caa7bf382e7e10c4351796331a14e9c5665a89
lanchunhui <zch921005@126.com>
2025-03-19 14:00:49 +0800

    add new ood dataset;

--- eval_tom/qwen_eval.sh
+++ eval_tom/qwen_eval.sh
@@ -8,10 +8,8 @@ TODAY=$(date +%Y%m%d)
 CKPTS_DIR="./checkpoints/GRPO_merge_tom/Qwen2.5-7B-Instruct-1M-5e-7-True-16/actor"
 
 DATA_PATHS=(
-    # "data/cleaned_tom/raw/ToM_train_600.parquet"
-    # "data/cleaned_tom/raw/hi_tom_3000.csv"
-    # "data/cleaned_tom/raw/Hi_ToM_cleaned.csv"
-    "./data/cleaned_tom/ToM_test_HiExTi_hint.parquet"
+    # "./data/cleaned_tom/ToM_test_HiExTi_hint.parquet"
+    "./eval_tom/ood_dataset/expert_tom_data.csv"
 )
 
 OUTPUT_DIR="./eval_tom/ckpts_results"
@@ -31,12 +29,14 @@ for CKPT_PATH in ${CKPTS_DIR}/*; do
         STEP="${CKPT_PATH##*_}"
         if [ ${STEP} -eq 700 ] || [ ${STEP} -eq 750 ] || [ ${STEP} -eq 800 ]; then
             for DATA_PATH in ${DATA_PATHS[@]}; do
-            # python3 eval_tom/qwen_series_eval.py --model_path ${CKPT_PATH} --data_path ${DATA_PATH} --output_dir ${OUTPUT_DIR}/ckpt_${STEP}.csv
-            python3 eval_tom/reasoning_model_eval.py \
-                --model_path ${CKPT_PATH} \
-                --data_path ${DATA_PATH} \
-                --output_dir ${OUTPUT_DIR}/ckpt_${STEP}_${TODAY}.csv \
-                --tp ${NUM_GPUS} $@ 2>&1 | tee ${LOG_DIR}/eval_${STEP}_${DATA_PATH}.log
+                BASENAME=$(basename ${DATA_PATH})
+                DATA_NAME="${BASENAME%.*}"
+                # python3 eval_tom/qwen_series_eval.py --model_path ${CKPT_PATH} --data_path ${DATA_PATH} --output_dir ${OUTPUT_DIR}/ckpt_${STEP}.csv
+                python3 eval_tom/reasoning_model_eval.py \
+                    --model_path ${CKPT_PATH} \
+                    --data_path ${DATA_PATH} \
+                    --output_dir ${OUTPUT_DIR}/ckpt_${STEP}_${DATA_NAME}_${TODAY}.csv \
+                    --tp ${NUM_GPUS} $@ 2>&1 | tee ${LOG_DIR}/eval_${STEP}_${DATA_PATH}.log
             done
         fi
     fi
